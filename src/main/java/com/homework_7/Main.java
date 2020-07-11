6214adf58456e38b1b45e84a978610d5bd2d7994
Sergiy D <SergiyDom@users.noreply.github.com>
2020-07-11 21:20:28 +0300

    Start to do Task_2

--- src/main/java/com/homework_7/Main.java
+++ src/main/java/com/homework_7/Main.java
@@ -1,4 +1,6 @@
-package com.homework_7.task_1_duplicate_with_string;
+package com.homework_7;
+
+import com.homework_7.task_1_duplicate_with_string.TaskOneManager;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/com/homework_7/task_2_family/Family.java
@@ -0,0 +1,4 @@
+package com.homework_7.task_2_family;
+
+public class Family {
+}
